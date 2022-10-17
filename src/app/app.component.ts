bc545fdfa791e16844d656c0fe6ea027aa2fa61e
ssj24 <sujee0824@gmail.com>
2022-10-17 21:11:51 +0900

    add section2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timestamp } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'starter-app';
-  name = 'tebah';
+  username = 'tebah';
+  show = false;
+  btnLogs = [];
+  resetInput() {
+    this.username = '';
+  }
+  detailBtnClicked() {
+    this.show = !this.show;
+    this.btnLogs.push(`${this.show} - ${new Date(Date.now())}`);
+  }
+  getBorder(i) {
+    if (i<5) return '1px solid red';
+    else return '3px double grey';
+  }
 }
